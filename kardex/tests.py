889414ea4979e289e67a8abfbf2c9bd0741a21ee
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2022-08-18 10:49:48 -0500

    Ya funcionan las pruebas unitarias

--- kardex/tests.py
+++ kardex/tests.py
@@ -1,7 +1,16 @@
 from django.test import TestCase
 
 
-class PruebaDeHumo(TestCase):
+class PruebaPortada(TestCase):
 
-    def test_suma_incorrecta(self):
-        self.assertEqual(1 + 1, 3)
+    def test_la_pagina_inicial_esta_funcionado(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_la_pagina_inicial_tiene_el_titulo_correcto(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Kardex App')
+
+    def test_la_portada_usa_la_plantilla_correcta(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'kardex/index.html')
--- kardexApp/urls.py
+++ kardexApp/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from kardex.views import PortadaView
 
 urlpatterns = [
+    path('', PortadaView.as_view(), name='portada'),
     path('admin/', admin.site.urls),
 ]
